a1186c0db7ac8e49b07e7fde4e1b7676095dc8d7
zubeydekarakus <142100002+zubeydekarakus@users.noreply.github.com>
2023-11-19 14:55:25 +0100

    Add files via upload

---
+++ CucumberPractice/src/test/java/mba_cucumber/pages/PageObject.java
@@ -0,0 +1,27 @@
+package mba_cucumber.pages;
+
+
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.PageFactory;
+import mba_cucumber.utilities.Driver;
+
+
+public class PageObject {
+    public PageObject(){
+        PageFactory.initElements(Driver.getDriver(), this);
+    }
+
+    public void scrollAndClick(WebElement element){
+        scrollInto(element);
+        element.click();
+    }
+
+    public void scrollInto(WebElement element){
+        JavascriptExecutor jse = (JavascriptExecutor) Driver.getDriver();
+
+        jse.executeScript("arguments[0].scrollIntoView();", element);
+    }
+
+    }
+
